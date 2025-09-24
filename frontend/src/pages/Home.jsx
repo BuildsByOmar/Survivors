4bdc94d3cf719746b0318a80633f16cb40344c67
ichraq ait addi <ichraqaitaddi@gmail.com>
2025-09-24 01:58:06 -1100

    page accueil

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,39 @@
+import React from "react";
+import Header from "./Header";
+
 export default function Home() {
   return (
-    <div className="max-w-3xl mx-auto mt-10">
-      <div className="flex justify-center w-full">
-        <h1 className="text-3xl font-bold mb-6 font-sans">Accueil du Forum</h1>
+    <>
+      
+      <div className="max-w-3xl mx-auto mt-10 px-4">
+        <div className="flex flex-col items-center gap-4"> 
+          {/* Titre */}
+          <h1 className="text-4xl font-bold font-sans text-center">
+            Survivors
+          </h1>
+
+          {/* Barre de recherche */}
+          <div className="w-full max-w-md relative">
+            <input
+              type="text"
+              placeholder="Entrez votre filtre de recherche..."
+              className="w-full px-8 py-9 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 pr-10"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="px-2 py-1 border border-gray-300 rounded-full hover:bg-gray-100 transition"
+          >
+            Rechercher
+          </button>
+        </div>
+
+        {/* filtre */}
+        <div className="mt-10">
+          {/* Posts */}
+        </div>
       </div>
-      {/* Filtres, liste des posts, système de like ici */}
-    </div>
+    </>
   );
 }
